054adef2e23ee3bd1a6659a502ca1a092f8b32bd
Luo Qingyong <luoqingyong@hotmail.com>
2023-03-19 00:57:12 +0800

    Initial release.

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# **exmail**
+批量发送个性化邮件给excel文件中的收件人。  
+
+
+## **准备工作**
+
+1. 如果你还没有安装python，首先请[下载python安装包](https://www.python.org/downloads/)并安装。
+2. 安装必要的python包  
+
+        pip install -r requirements.txt  
+   
+
+3. 定制邮件模板mail_template.html。在需要的地方输入要替换的变量。变量格式为 {{变量名称}}。  
+
+    比如，如果模板文件里有一个变量 {{姓名}}，程序执行的时候会使用Excel里“姓名”这一列的值替换模板中的 {{姓名}} 这几个字符。  
+
+    ![img](images/template-var.png)
+  
+
+4. 修改config.ini中的邮件服务器等配置信息。  
+
+![img](images/config-email.png)  
+
+    注意：
+    - 把其中 email_col的值改成excel文件中存放电子邮件地址这一列的列名。在上面的例子中，列名是“邮箱”。
+    - 使用网易邮箱时，需要开通POP3/SMTP权限，并用开通权限后获得的token字符串作为smtp_password参数的值。
+  
+
+## **发送邮件**
+
+执行程序：   
+
+    python app.py [--temp_file TEMP_FILE] excel_file 
+  
+其中：  
+- excel_file 是存放收件人信息的excel文件。  
+- TEMP_FILE 是邮件模板文件的名称，缺省是email_template.html。
+  
+
+
+## **检查并处理发送结果**
+程序执行完成之后会生成一个results.xlsx文件。此文件内容与输入的excel文件一致，只是增加了一列“邮件已发送”。这一列的值为“Y”表示该收件人的邮件已经成功发送，值为“N”表示还没有发送/发送不成功。  
+  
+使用下列命令可以继续发送邮件，其中“邮件已发送”列的值为“Y”的行会跳过，实现断点重发的效果。
+  
+    python app.py --temp_file email_template.html results.xlsx 
---
+++ app.py
@@ -0,0 +1,106 @@
+import argparse
+import pandas as pd
+import smtplib
+import openpyxl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+#from email.mime.application import MIMEApplication
+from jinja2 import Template
+import configparser
+
+# 从命令行参数中读取 Excel 文件名
+parser = argparse.ArgumentParser(description='发送邮件给Excel文件中的收件人。')
+parser.add_argument('excel_file', help='包含收件人列表的Excel文件')
+parser.add_argument('--temp_file', required=False, default='email_template.html', help='邮件模板文件，HTML格式，缺省为email_template.html')
+args = parser.parse_args()
+
+# 读取 Excel 文件
+try:
+    df = pd.read_excel(args.excel_file)
+
+    # 检查是否有发送状态列，如果没有，则增加状态列并把发送状态设置为"N"
+    if "邮件已发送" not in df.columns:
+        df["邮件已发送"]="N"
+
+except (FileNotFoundError, openpyxl.utils.exceptions.InvalidFileException) as e:
+    print(f'读取Excel文件出错: {e}')
+    exit()
+
+# 从配置文件中读取配置信息
+try:
+    with open('config.ini') as f:
+        config = configparser.ConfigParser()
+        config.read_file(f)
+
+        # 读取 SMTP 服务器和认证信息
+        smtp_server = config['SMTP']['smtp_server']
+        smtp_port = int(config['SMTP']['smtp_port'])
+        smtp_username = config['SMTP']['smtp_username']
+        smtp_password = config['SMTP']['smtp_password']
+
+        # 读取邮件信息
+        subject = config['MAIL']['subject']
+        email_col = config['MAIL']['email_col']
+except IOError:
+    print("读取config.ini出错。")
+    exit()
+
+# 打开邮件模板
+try:
+    with open(args.temp_file, 'r') as f:
+        template = Template(f.read())
+except IOError:
+    print("读取{}出错。".format(args.temp_file))
+    exit()
+
+# 登录邮件服务器
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+
+    # 循环遍历每个收件人
+    for index, row in df.iterrows():
+
+        # 如果邮件已经发过了，则跳过这条记录。
+        if row["邮件已发送"] != "N":
+            continue
+
+        email_addr = row[email_col]
+
+        # 配置邮件头信息
+        message = MIMEMultipart()
+        message['From'] = smtp_username
+        message['To'] = email_addr
+        message['Subject'] = subject
+
+        # 生成邮件正文内容
+        # 订单编号，姓名，手机号，邮箱，俱乐部名称，所属中区
+        # html = template.render(order_no=row['订单编号'], name=row['姓名'], phone=row['手机号'], email=row['邮箱'], 
+        #                         club=row['俱乐部名称'], zone=row['所属中区'])
+        data = dict(row)
+        html = template.render(data)
+
+        # 将 HTML 正文作为邮件内容
+        body = MIMEText(html, 'html')
+        message.attach(body)
+
+        # 发送邮件
+        try:
+            server.sendmail(smtp_username, email_addr, message.as_string())
+
+            # 设置发送状态
+            df.loc[index, "邮件已发送"] = "Y"
+        except Exception as e:
+            print("Error sending mail to {}".format(email_addr))
+
+        if index >= 0:
+            break
+
+        if (index+1) % 50 == 0:
+            print("{} records have been processed.".format(index+1))
+
+
+# 保存发送结果
+df.to_excel("results.xlsx", index=False)
+
+print("总共 {} 个收件人，发送了 {} 封邮件。".format(len(df), len(df[df["邮件已发送"]=="Y"])))
